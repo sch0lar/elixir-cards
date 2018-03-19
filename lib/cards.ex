25ae74b57a8fa7f789ae7993dcb5e06c3f7d1acf
Saad Anjum <saad.anjum@thinkresearch.com>
2018-03-19 14:05:57 -0400

    Learn Elixir: Implement sample Cards project

---
+++ lib/cards.ex
@@ -0,0 +1,50 @@
+defmodule Cards do
+  @moduledoc """
+  Provides methids for creating and handling a deck of cards
+"""
+
+  @doc """
+  returns a list of strings representing a deck
+"""
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four","Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  @doc """
+   Divides a deck into a hand and the remainder of the deck.
+"""
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary}  -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
+end
